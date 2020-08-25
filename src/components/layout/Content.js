b92d499178f097f0e73fc525605fb6813b5e3eb5
sts2003 <44109148+sts2003@users.noreply.github.com>
2020-08-25 14:12:52 +0900

    main Content

--- src/components/layout/Content.js
+++ src/components/layout/Content.js
@@ -1,14 +1,15 @@
 import React from "react";
 
-
 class Content extends React.Component {
-    render() {
-        return (
-            <div>
-                Content layout
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="Content">
+        <div className="Content__main-img"></div>
+        <div></div>
+        <div></div>
+      </div>
+    );
+  }
 }
 
 export default Content;
