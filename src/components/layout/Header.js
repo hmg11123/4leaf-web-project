d9be34d5ff00c57d191319ca8f804405f8faa509
hyj11123 <43002611+hyj11123@users.noreply.github.com>
2020-08-25 12:23:33 +0900

    logo

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,36 @@
 import React from "react";
-
+import logo from "../images/longLoge.png";
+import { HashRouter as Router, Link } from 'react-router-dom';
 
 class Header extends React.Component {
     render() {
         return (
-            <div>
-                Header layout
+            <div className="Header">
+                <Router>
+                    <Link to="/">
+                        <img src={logo} className="logo" />
+                    </Link>
+                </Router>
+
+                <ul className="login">
+                    <li>
+                        <input type="button" value="SIGN IN" className="h-btn" />
+                    </li>
+                    <li>
+                        <input type="button" value="SIGN UP" className="h-btn" />
+                    </li>
+                </ul>
+                <ul className="main">
+                    <li>
+                        <input type="button" value="회사소개" className="h-btn" />
+                    </li>
+                    <li>
+                        <input type="button" value="의뢰하기" className="h-btn" />
+                    </li>
+                    <li>
+                        <input type="button" value="문의하기" className="h-btn" />
+                    </li>
+                </ul>
             </div>
         )
     }
