fae9aa42402346614f0e564f6f416b74fabe0d8c
hyj11123 <43002611+hyj11123@users.noreply.github.com>
2020-08-25 13:55:52 +0900

    header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -10,30 +10,30 @@ class Header extends React.Component {
                     <Link to="/">
                         <img src={logo} className="Header__logo" />
                     </Link>
-                </Router>
 
-                <ul className="login">
-                    <li>
-                        <input type="button" value="SIGN IN" className="h-btn" />
-                    </li>
-                    <li>
-                        <input type="button" value="SIGN UP" className="h-btn" />
-                    </li>
-                </ul>
-                <ul className="main">
-                    <li>
-                        <input type="button" value="회사소개" className="h-btn" />
-                    </li>
-                    <li>
-                        <input type="button" value="의뢰하기" className="h-btn" />
-                    </li>
-                    <li>
-                        <input type="button" value="가격안내" className="h-btn" />
-                    </li>
-                    <li>
-                        <input type="button" value="문의하기" className="h-btn" />
-                    </li>
-                </ul>
+                    <ul className="login">
+                        <li className="h-btn">
+                            <Link to="/"  >SIGN IN</Link>
+                        </li>
+                        <li className="h-btn">
+                            <Link to="/"  >SIGN UP</Link>
+                        </li>
+                    </ul>
+                    <ul className="main">
+                        <li className="h-btn">
+                            <Link to="/"  >회사소개</Link>
+                        </li>
+                        <li className="h-btn">
+                            <Link to="/"  >개발의뢰</Link>
+                        </li>
+                        <li className="h-btn">
+                            <Link to="/"  >가격안내</Link>
+                        </li>
+                        <li className="h-btn">
+                            <Link to="/"  >문의하기</Link>
+                        </li>
+                    </ul>
+                </Router>
             </div>
         )
     }
