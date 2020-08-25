604742c3d670fcea6c097a7d889e4b3ff646dda7
minhyoungo <59827905+minhyoungo@users.noreply.github.com>
2020-08-25 13:55:15 +0900

    nav

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import React from "react";
-import { Route } from 'react-router-dom';
-import { Header, Footer, Content } from "./components/layout"
+import { Route } from "react-router-dom";
+import { Header, Footer, Content, Navigation } from "./components/layout";
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="app">
+  render() {
+    return (
+      <div className="app">
+        <header>
+          <div className="header">
+            <Route exact path="/" component={Header} />
+          </div>
+        </header>
+        <main>
+          <div className="Content">
+            <Route exact path="/" component={Content} />
+          </div>
 
-                <header>
-                    <div className="header">
-                        <Route exact path="/" component={Header} />
-                    </div>
-                </header>
-                <main>
-                    <div className="Content">
-                        <Route exact path="/" component={Content} />
-                    </div>
-                </main>
-                <footer>
-                    <div className="footer">
-                        <Route exact path="/" component={Footer} />
-                    </div>
-                </footer>
-            </div>
-        );
-    }
+          <div className="Navigation">
+            <Route exact path="/" component={Navigation} />
+          </div>
+        </main>
+        <footer>
+          <div className="footer">
+            <Route exact path="/" component={Footer} />
+          </div>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -1,31 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
-import "./Navigation.css";
 
-function Navigation() {
-  return (
-    <div className="nav">
-      <Link className="" to="">
-        <FiberManualRecordIcon style={{ color: green[500] }} fontSize="large" />
-      </Link>
-      <Link className="" to="">
-        <FiberManualRecordIcon />
-      </Link>
-      <Link className="" to="">
-        <FiberManualRecordIcon />
-      </Link>
-      <Link className="" to="">
-        <FiberManualRecordIcon />
-      </Link>
-      <Link className="" to="">
-        <FiberManualRecordIcon />
-      </Link>
-      <Link className="" to="">
-        <FiberManualRecordIcon />
-      </Link>
-    </div>
-  );
+class Navigation extends React.Component {
+  render() {
+    return (
+      <div className="navs">
+        <Link className="navs__siginIn nav" to="signIn">
+          <FiberManualRecordIcon />
+        </Link>
+
+        <Link className="navs__signUp nav" to="signUp">
+          <FiberManualRecordIcon />
+        </Link>
+
+        <Link className="navs__info nav" to="info">
+          <FiberManualRecordIcon />
+        </Link>
+
+        <Link className="navs__price nav" to="price">
+          <FiberManualRecordIcon />
+        </Link>
+
+        <Link className="navs__request nav" to="request">
+          <FiberManualRecordIcon />
+        </Link>
+
+        <Link className="navs__ask nav" to="ask">
+          <FiberManualRecordIcon />
+        </Link>
+      </div>
+    );
+  }
 }
 
 export default Navigation;
