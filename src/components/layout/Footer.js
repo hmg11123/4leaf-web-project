548adab5997a5101cb78626e189a0ade42ba8ee2
sts2003 <44109148+sts2003@users.noreply.github.com>
2020-08-25 12:00:33 +0900

    css doing

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,14 +1,23 @@
 import React from "react";
 
-
 class Footer extends React.Component {
-    render() {
-        return (
-            <div>
-                Footer layout
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="Footer">
+        <div className="Footer__left"></div>
+        <div className="Footer__center">
+          <ul>
+            <li>4LEAF SOFTWARE</li>
+            <li>대표 윤 상 호</li>
+            <li>4leaf.ysh@gmail.com</li>
+            <li>위치 : 대전광역시 서구 계룡로 394번길 14-14</li>
+            <li>Copyright ⓒ 2020 All rights reserved. By 4LEAF SOFTWARE</li>
+          </ul>
+        </div>
+        <div className="Footer__right"></div>
+      </div>
+    );
+  }
 }
 
 export default Footer;
