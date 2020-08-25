582d8448ce5dd26c958440c3777c53a85be7dc3a
hyj11123 <43002611+hyj11123@users.noreply.github.com>
2020-08-25 15:13:11 +0900

    finish

--- src/components/layout/Content.js
+++ src/components/layout/Content.js
@@ -4,9 +4,15 @@ class Content extends React.Component {
   render() {
     return (
       <div className="Content">
-        <div className="Content__main-img"></div>
-        <div></div>
-        <div></div>
+        <div className="Content__main">
+          <div className="Content__main-img"></div>
+        </div>
+        <div className="Content__main">
+          <div className="Contnet__main-img-2"></div>
+        </div>
+        <div className="Content__main">
+          <div className="Content__main-img-3"></div>
+        </div>
       </div>
     );
   }
